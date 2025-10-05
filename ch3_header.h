d41628cdbab746030556f276a5041093a07454c3
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2025-10-06 00:55:10 +0900

    Update ch3_header.h

--- ch3_header.h
+++ ch3_header.h
@@ -1,22 +1,21 @@
-//전사-고블린
-//#ifndef __CHARACTOR_H__
-//#define __CHARACTOR_H__
-//#include <string>
-//using namespace std;
-//
-//class Charactor {
-//private:
-//	string name;
-//	int hp;
-//	int attackPower;
-//
-//public:
-//	void InitMembers(const string name, int hp, int power);
-//	void ShowStatus();
-//	void Attack(Charactor& target);
-//	void TakeDamage(int damage);
-//	bool IsDead();
-//};
-//
-//#endif
+#ifndef __CHARACTOR_H__
+#define __CHARACTOR_H__
+#include <string>
+using namespace std;
+
+class Charactor {
+private:
+	string name;
+	int hp;
+	int attackPower;
+
+public:
+	void InitMembers(const string name, int hp, int power);
+	void ShowStatus();
+	void Attack(Charactor& target);
+	void TakeDamage(int damage);
+	bool IsDead();
+};
+
+#endif
 
