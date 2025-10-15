0e1fb16ec94fd1cad2102f1cca1e9ca81c96a9a1
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2025-10-15 17:22:42 +0900

    Add files via upload

---
+++ ch5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class DynamicArray {
+private:
+	int* data;
+	int size;
+
+public:
+	DynamicArray(int n) : size(n) {
+		data = new int[n];
+		for (int i = 0; i < n; i++) data[i] = i;
+	}
+
+	DynamicArray(const DynamicArray& copy) : size(copy.size) {
+		data = new int[copy.size];
+		for (int i = 0; i < copy.size; i++) data[i] = i;
+	}
+
+	void Set(int idx, int val) {
+		if (idx >= 0 && idx < size)data[idx] = val;
+	}
+	
+	void Print() {
+		for (int i = 0; i < size; i++)cout << data[i] << " ";
+		cout << '\n';
+	}
+
+	~DynamicArray() {
+		delete[] data;
+	}
+};
+
+int main() {
+	DynamicArray arr1(5);
+	DynamicArray arr2 = arr1;
+	arr2.Set(0, 100);
+
+	cout << "arr1: ";
+	arr1.Print();
+	cout << "arr2: ";
+	arr2.Print();
+
+	return 0;
+}
