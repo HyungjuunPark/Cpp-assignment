72c74f0d7957da424aebc69f5ef11e4e6920a71f
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2025-10-21 17:23:15 +0900

    Update ch4_1.cpp

--- ch4_1.cpp
+++ ch4_1.cpp
@@ -1,78 +1,80 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
-class Book {
+class Book
+{
 private:
-	int bookID;
-	char* title;
-	int stock;
+    int bookID;
+    char* title;
+    int stock;
 
 public:
-	// 생성자: strlen, strcpy 없이 깊은 복사 구현
-	Book(int bookID, const char* title, int stock) {
-		this->bookID = bookID;
-		this->stock = stock;
+    // 생성자: 이니셜라이저 리스트와 깊은 복사 사용
+    Book(int ID, const char* bookTitle, int bookStock) : bookID(ID), stock(bookStock)
+    {
+        // 제목을 위한 메모리를 동적 할당하고, 문자열을 복사 (깊은 복사)
+        title = new char[strlen(bookTitle) + 1];
+        //strcpy(title, bookTitle);
+        strcpy_s(title, strlen(bookTitle) + 1, bookTitle);        
+    }
 
-		// 1. 문자열 길이 직접 계산
-		int len = 0;
-		while (title[len] != '\0') {
-			len++;
-		}
+    // 소멸자: 동적 할당된 메모리 해제
+    ~Book()
+    {
+        delete[] title;
+    }
 
-		// 2. 힙 메모리 동적 할당 (널 문자를 포함하여 len + 1)
-		this->title = new char[len + 1];
+    // 도서 번호를 반환하는 const 멤버 함수
+    int GetBookID() const
+    {
+        return bookID;
+    }
 
-		// 3. 문자열 내용 직접 복사 (깊은 복사)
-		int i = 0;
-		while (i <= len) { // 널 문자까지 복사하기 위해 <= len
-			this->title[i] = title[i];
-			i++;
-		}
-	}
+    // 도서 대여 함수
+    void BorrowBook()
+    {
+        if (stock > 0)
+        {
+            stock--;
+            cout << "'" << title << "' 도서를 대여했습니다." << endl;
+        }
+        else
+        {
+            cout << "재고가 부족하여 '" << title << "' 도서를 대여할 수 없습니다." << endl;
+        }
+    }
 
-	// 소멸자: 힙 메모리 해제
-	~Book() {
-		delete[] title;
-	}
+    // 도서 반납 함수
+    void ReturnBook()
+    {
+        stock++;
+        cout << "'" << title << "' 도서를 반납했습니다." << endl;
+    }
 
-	int GetBookID() const {
-		return bookID;
-	}
-
-	void BorrowBook() {
-		if (stock >= 1) {
-			cout << "'" << title << "' 도서를 대여했습니다.\n";
-			stock--;
-		}
-		else
-			cout << "재고가 부족하여 대여할 수 없습니다.\n";
-	}
-
-	void ReturnBook() {
-		stock++;
-	}
-
-	void ShowBookInfo() const {
-		cout << "--- 도서 정보 ---\n";
-		cout << "도서 번호: " << bookID << '\n';
-		cout << "제	목: " << title << '\n'; // 올바르게 출력됨
-		cout << "재고 수량: " << stock << "권\n";
-	}
+    // 도서 정보를 출력하는 const 멤버 함수
+    void ShowBookInfo() const
+    {
+        cout << "--- 도서 정보 ---" << endl;
+        cout << "도서 번호: " << bookID << endl;
+        cout << "제    목: " << title << endl;
+        cout << "재고 수량: " << stock << "권" << endl;
+    }
 };
 
 int main() {
-	// 상수 문자열 리터럴을 안전하게 전달
-	Book oop_book(101, "C++ Programming", 5);
-	oop_book.ShowBookInfo();
+    Book oop_book(101, "C++ Programming", 5);
+    oop_book.ShowBookInfo();
 
-	oop_book.BorrowBook();
-	oop_book.BorrowBook();
-	cout << "\n## 2권 대여 후 ##" << '\n';
-	oop_book.ShowBookInfo();
+    oop_book.BorrowBook();
+    oop_book.BorrowBook();
+    cout << "\n## 2권 대여 후 ##" << endl;
+    oop_book.ShowBookInfo();
 
-	oop_book.ReturnBook();
-	cout << "\n## 1권 반납 후 ##" << '\n';
-	oop_book.ShowBookInfo();
+    oop_book.ReturnBook();
+    cout << "\n## 1권 반납 후 ##" << endl;
+    oop_book.ShowBookInfo();
 
-	return 0;
+    return 0;
 }
